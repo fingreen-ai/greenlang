66bac4ecc70b07dec2a1f4f116bbf54b01ddd102
Alban Tiberghien <alban.tiberghien@fingreen.ai>
2024-01-26 16:03:28 +0100

    Add scope 3 category 12

---
+++ calculations/ghg/scope_3/forms/end_of_life_treatment_of_sold_products.py
@@ -0,0 +1,48 @@
+""" EndOfLifeTreatment form """
+from django import forms
+from django.utils.translation import gettext_lazy as _
+
+from crispy_forms.layout import Column, Field, Row
+from crispy_forms.bootstrap import AppendedText
+from numpy import place
+
+from fingreen_web.models import CollectionItem
+
+from ...base.forms import PredefinedFactorCalculationMethodForm
+
+
+class EndOfLifeTreatmentForm(PredefinedFactorCalculationMethodForm):
+    """EndOfLifeTreatment form """
+
+    sold_quantity = forms.IntegerField(required=True)
+
+    def get_extra_fields(self):
+        return {
+            Column(
+                Field(
+                    'sold_quantity',
+                    placeholder=_("Sold quantity"),
+                    min=0,
+                    type="number",
+                    autocomplete="off",
+                    css_class="form-control form-control-lg mb-3 mb-lg-0",
+                ),
+                css_class="col-2",
+            ),
+        }
+
+    def save(self, commit=True):
+        """Save form"""
+
+        instance = super().save(commit=False)
+
+        instance.widget_data = {
+            'sold_quantity': self.cleaned_data["sold_quantity"],
+        }
+
+        if commit:
+            instance.save()
+
+        return instance
+    
+
